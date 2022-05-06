896b5ced8a4476ffd103a4a04da1918a7843b961
benito <benideveloperr@gmail.com>
2022-05-06 20:30:14 -0300

    Adding settings for heroku

--- database/db.js
+++ database/db.js
@@ -4,7 +4,8 @@ import { DB_URI, DB_URI_TEST, NODE_ENV } from "../config.js";
 export const connectDB = async () => {
   try {
     const db = await mongoose.connect(
-      NODE_ENV === "test" ? DB_URI_TEST : DB_URI
+      NODE_ENV == "test" ? DB_URI_TEST : DB_URI,
+      { useNewUrlParser: true, useCreateIndex: true }
     );
     console.log("connect to", db.connection.name);
   } catch (err) {
